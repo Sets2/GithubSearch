be190477c0ddd8a9b57f6735d9589cf71d88bc85
Sets2 <sah76@mail.ru>
2023-01-23 15:57:15 +0300

    pagination 2

--- GithubSearch/Models/Pagination.cs
+++ GithubSearch/Models/Pagination.cs
@@ -2,10 +2,10 @@
 {
     public class Pagination
     {
-        public int pageNumber;          // Номер страницы (из параметров пагинации)
-        public int pageSize;            // Размер страницы (из параметров пагинации)
-        public int totalPages;          // Всего страниц при такой пагинации
-        public int totalCount;          // Всего репозиториев 
-        public int totalGitResponse;    // Всего записей в БД
+        public int pageNumber { get; set; }          // Номер страницы (из параметров пагинации)
+        public int pageSize { get; set; }            // Размер страницы (из параметров пагинации)
+        public int totalPages { get; set; }          // Всего страниц при такой пагинации
+        public int totalCount { get; set; }          // Всего репозиториев 
+        public int totalGitResponse { get; set; }    // Всего записей в БД
     }
 }
